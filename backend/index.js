95c01ada6ab208d91795a06ef494e06b84aaa216
AnoopRepo <divyanshtripathi201@gmail.com>
2026-06-18 19:15:33 +0530

    deploy frontend

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.post(
 )
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-examnotesclient.onrender.com",
     credentials: true,
     methods: ["GET", "PUT", "DELETE", "POST", "OPTIONS"],
   }),
